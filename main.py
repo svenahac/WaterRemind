50e61a455d24abfbc12feb4b740df911927d0ef0
vexane <ahacsh@gmail.com>
2021-05-29 18:05:35 +0200

    V0.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import time
+import time as mytime
+import  pygame
 
 root = Tk()
 root.title("WaterRemind")
@@ -7,50 +8,90 @@ root.geometry("300x300")
 root.resizable(False, False)
 root.iconbitmap("icon.ico")
 
+# Initializes pygame mixer 
+pygame.mixer.init()
+
+# Dictionary of all available sounds
+sounds = {
+    "Default" : "audio/default.mp3",
+    "Amelia" : "audio/amelia.mp3",
+    "Bruh" : "audio/bruh.mp3"
+}
+
 # Frame 1
 frame = Frame(root)
 frame.pack()
 
-def start_timer():
-    ''' Starts timer '''
-    t = int(time_entry.get())
-    
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins,secs)
-        time2 = Label(frame2, text=timer, font=("Helvetica", 18))
-        time2.grid(row=0, column=1)
-        frame.after(1000)
-        t -= 1
-    
+def timer():
+    ''' First removes elements then starts timer, when it ends it adds elements back'''
+    time.grid_remove()
+    time_entry.grid_remove()
+    repeats.grid_remove()
+    repeats_entry.grid_remove()
+    startTimer.grid_remove()
+    timer_running.grid(row=0, column=0)
 
+    # Selects the sound notification
+    sound_file = sounds[clicked.get()]
+
+    n = 0
+    our_repeats = int(repeats_entry.get())
+    while our_repeats > n:
+        time_now = int(time_entry.get())
+        root.update()
+        mytime.sleep(time_now)
+        pygame.mixer.music.load(sound_file)
+        pygame.mixer.music.play(loops=0)
+        mytime.sleep(2)
+        n += 1
+    if n == our_repeats:
+        timer_running.grid_remove()
+        time.grid()
+        time_entry.grid()
+        repeats.grid()
+        repeats_entry.grid()
+        startTimer.grid()
+
+# Text that shows when timer is running
+timer_running = Label(frame, text="Timer is running", font=("Helvetica", 20))
 
 # Text and entry for time
-time = Label(frame, text="Set time (min): ", font=("Helvetica", 18))
+time = Label(frame, text="Set time (seconds): ", font=("Helvetica", 14))
 time.grid(row=0, column=0)
 time_entry = Entry(frame, width=15)
-time_entry.insert(0, 30)
+time_entry.insert(0, 1800)
 time_entry.grid(row=0, column=1, columnspan=2)
-old_time = int(time_entry.get())
 
-startTimer = Button(frame, text="Start Timer", command=start_timer, font=("Helvetica", 18), padx=2, pady=2)
-startTimer.grid(row=1, column=0, columnspan=3)
+# Text and entry for number of repeats
+repeats = Label(frame, text="Repeats: ", font=("Helvetica", 14))
+repeats.grid(row=1, column=0)
+repeats_entry = Entry(frame, width=15)
+repeats_entry.insert(0, 1)
+repeats_entry.grid(row=1, column=1, columnspan=2)
+
+# Button that starts the timer
+startTimer = Button(frame, text="Start Timer", command=timer, font=("Helvetica", 14), padx=2, pady=2)
+startTimer.grid(row=2, column=0, columnspan=3)
 
 # Black line
 my_canvas = Canvas(root, width=300, height=2, bg="black")
 my_canvas.pack()
 
-def start_timer():
-    while hours != 0 and minutes != 0 and seconds != 0:
-        seconds -= 1
-        time.after(1000,start_timer)
-
 # Frame 2
 frame2 = Frame(root)
 frame2.pack()
 
-timer = Label(frame2, text="Timer:", font=("Helvetica", 18))
-timer.grid(row=0, column=0)
+sound = Label(frame2, text="Select sound", font=("Helvetica", 14))
+sound.grid(row=0, column=0)
+
+clicked = StringVar()
+clicked.set("Default")
+
+options = {"Default" : "hello", "Amelia" : "Juhu"}
+
+dropdown = OptionMenu(frame2, clicked, "Default", "Amelia", "Bruh")
+dropdown.grid(row=0, column=1, columnspan=2)
+
 
 # Black line
 my_canvas2 = Canvas(root, width=300, height=2, bg="black")
